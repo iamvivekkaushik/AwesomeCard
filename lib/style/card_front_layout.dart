ec7a26e2875e93caa0311753b8cbcf6499f48790
vivek <kaushikvivek.17@gmail.com>
2019-12-06 14:36:32 +0530

    Fixed Issue with card height and text being cut out on different devices

--- lib/style/card_front_layout.dart
+++ lib/style/card_front_layout.dart
@@ -22,7 +22,7 @@ class CardFrontLayout {
 
   Widget layout1() {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+      padding: const EdgeInsets.fromLTRB(20, 8, 20, 20),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -61,87 +61,82 @@ class CardFrontLayout {
               ),
             ],
           ),
-          SizedBox(
-            height: 30,
-          ),
-          Container(
-            width: cardWidth,
+          Expanded(
             child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  cardNumber == null || cardNumber.isEmpty
-                      ? 'XXXX XXXX XXXX XXXX'
-                      : cardNumber,
-                  style: TextStyle(
-                      package: 'awesome_card',
-                      color: textColor,
-                      fontWeight: FontWeight.w500,
-                      fontFamily: "MavenPro",
-                      fontSize: 22),
-                )),
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        "Exp. Date",
-                        style: TextStyle(
-                            package: 'awesome_card',
-                            color: textColor,
-                            fontFamily: "MavenPro",
-                            fontSize: 15),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        cardExpiry == null || cardExpiry.isEmpty
-                            ? "MM/YY"
-                            : cardExpiry,
-                        style: TextStyle(
-                            package: 'awesome_card',
-                            color: textColor,
-                            fontWeight: FontWeight.w500,
-                            fontFamily: "MavenPro",
-                            fontSize: 16),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Text(
-                    cardHolderName == null || cardHolderName.isEmpty
-                        ? "Card Holder"
-                        : cardHolderName,
-                    style: TextStyle(
-                        package: 'awesome_card',
-                        color: textColor,
-                        fontWeight: FontWeight.w500,
-                        fontFamily: "MavenPro",
-                        fontSize: 17),
-                  ),
-                ],
-              ),
-              Expanded(
-                child: Align(
-                  alignment: Alignment.bottomRight,
-                  child: cardTypeIcon,
+              alignment: Alignment.bottomLeft,
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          cardNumber == null || cardNumber.isEmpty
+                              ? 'XXXX XXXX XXXX XXXX'
+                              : cardNumber,
+                          style: TextStyle(
+                              package: 'awesome_card',
+                              color: textColor,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: "MavenPro",
+                              fontSize: 22),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "Exp. Date",
+                              style: TextStyle(
+                                  package: 'awesome_card',
+                                  color: textColor,
+                                  fontFamily: "MavenPro",
+                                  fontSize: 15),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              cardExpiry == null || cardExpiry.isEmpty
+                                  ? "MM/YY"
+                                  : cardExpiry,
+                              style: TextStyle(
+                                  package: 'awesome_card',
+                                  color: textColor,
+                                  fontWeight: FontWeight.w500,
+                                  fontFamily: "MavenPro",
+                                  fontSize: 16),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          cardHolderName == null || cardHolderName.isEmpty
+                              ? "Card Holder"
+                              : cardHolderName,
+                          style: TextStyle(
+                              package: 'awesome_card',
+                              color: textColor,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: "MavenPro",
+                              fontSize: 17),
+                        ),
+
+                      ],
+                    ),
+                    cardTypeIcon
+                  ],
                 ),
-              )
-            ],
+              ),
+            ),
           ),
         ],
       ),
